e86417603b163c160ce6a84e81cc43f6c6212c17
RaniDahihande11 <131444270+RaniDahihande11@users.noreply.github.com>
2025-10-30 18:09:21 +0530

    testing2

--- read_data_api.py
+++ read_data_api.py
@@ -6,5 +6,5 @@ import pandas as pd
 response = requests.get('https://jsonplaceholder.typicode.com/users')
 data = response.json()
 df = pd.DataFrame(data)
-
+df = df ['id','name'] # want to show only id and name, this is called transformation
 print(df)
