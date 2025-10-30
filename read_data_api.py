e32c10653cfb5d7d579d5ef89e7d975a02597458
RaniDahihande11 <131444270+RaniDahihande11@users.noreply.github.com>
2025-10-30 18:29:35 +0530

    testing4

--- read_data_api.py
+++ read_data_api.py
@@ -1,10 +1,14 @@
 import requests
 import pandas as pd
-
+import os
 #  to call api use --> requests
 
-response = requests.get('https://jsonplaceholder.typicode.com/users')
-data = response.json()
-df = pd.DataFrame(data)
-df = df [['id','name']] # want to show only id and name, this is called transformation
-print(df)
+token = os.getenv("API_TOKEN") # for private api
+print(f"Toekn: {token}")
+
+
+# response = requests.get('https://jsonplaceholder.typicode.com/users')
+# data = response.json()
+# df = pd.DataFrame(data)
+# df = df [['id','name']] # want to show only id and name, this is called transformation
+# print(df)
